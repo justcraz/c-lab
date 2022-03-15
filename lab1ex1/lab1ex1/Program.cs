03c6709073a0523a638596279e2c055614ca46c9
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2022-03-15 12:27:31 +0200

    first commit

---
+++ lab1ex1/lab1ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int num, last;
+
+        // Reading number
+        Console.Write("Введiть трьохзначне число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        last = num % 10;
+        Console.WriteLine("Остання цифра введеного числа: " + last);
+
+        while (num >= 10)
+        {
+            num = num / 10;
+        }
+
+        Console.WriteLine("Перша цифра введеного числа: " + num);
+        Console.ReadLine();
+    }
+}
